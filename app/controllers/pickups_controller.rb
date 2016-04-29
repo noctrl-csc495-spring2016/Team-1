77161b7e235d5b755d792aba9f8226498228266a
Rachael Rayas <rlrayas@noctrl.edu>
2016-04-29 17:38:49 +0000

    Updating repository for correct inforamtion

--- app/controllers/pickups_controller.rb
+++ app/controllers/pickups_controller.rb
@@ -1,3 +1,11 @@
 class PickupsController < ApplicationController
-
+    def index
+        #need the month and year for the correct pull
+        
+      @pickups = Pickup.order(:day_id)
+      respond_to do |format|
+          format.html
+          format.csv { send_data @pickups.to_csv }
+      end
+  end
 end
--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -1,4 +1,13 @@
 class Pickup < ActiveRecord::Base
   belongs_to :follower, class_name: "Day"
+  attr_accessible :donor_name, :donor_address_line1, :donor_city, :donor_state, :donor_zip, :donor_email, :pickup_time, :item_description
   
+  def self.to_csv(options = {})
+      CSV.generate(options) do |csv|
+          csv. << column_names
+          all.each do |pickup|
+              csv << pickup.attributes.values_at(*column_names)
+          end
+      end
+  end
 end

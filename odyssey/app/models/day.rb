4f0d99928f59ceea2c615ad49d69d3e4118ecf8a
eric zuidema <erzuidema@noctrl.edu>
2016-04-29 12:22:59 -0500

    Initial commit

--- odyssey/app/models/day.rb
+++
@@ -1,4 +0,0 @@
-class Day < ActiveRecord::Base
-  has_many :pickups, class_name: "Pickup"
-  
-end
--- odyssey/db/migrate/20160424220218_change_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration
-  def change
-    rename_column :users, :perission_level, :permission_level
-  end
-end

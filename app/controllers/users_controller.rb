0fbd3ec98d0f3734c5377dfad23e9f976bba97c4
Bill Moers <moers4@gmail.com>
2016-05-04 20:39:53 +0000

    Added form validation and flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,11 @@ class UsersController < ApplicationController
   #then displays the new list of users.
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.valid? && @user.save
       flash.now[:success] = "Acount created!"
-      redirect_to action: 'index'
+      render 'index'
     else
+      flash.now[:danger] = "One or more values was left blank!"
       render 'new'
     end
   end
@@ -37,9 +38,19 @@ class UsersController < ApplicationController
   #Finds the user to edit and saves the new parameters.
   def update
     @user = User.find(params[:id])
+    
+    #If the password text area was blank
     if params[:user][:user_password_digest].empty?
       @user.errors.add(:user_password_digest, "can't be empty")
+      flash.now[:danger] = "Password can't be empty!"
       render 'edit'
+      
+    #If the permission level was blank
+    elsif params[:user][:permission_level].empty?
+      @user.errors.add(:permission_level, "can't be blank")
+      flash.now[:danger] = "Permission level required!"
+      render 'edit'
+    
     elsif @user.update_attributes(user_params)
       flash.now[:success] = "Profile updated."
       redirect_to action: 'index'

27ad3b9ab6317e629935528769ad41fc36cb7676
Rachael Rayas <rlrayas@noctrl.edu>
2016-05-10 21:47:05 +0000

    reports complete

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,11 +8,11 @@ class ReportsController < ApplicationController
         @date = params[:date]
         #I have no idea why I can't say params[:date]['month'], heres the work around
         @arr = @date.to_s.split(",")
-        @month = @arr[0].split("\"")[3]
-        if @month.length == 1
+        @month = @arr[0].to_s.split("\"")[3]
+        if @month.to_s.length == 1
           @month.insert(0,'0')
         end
-        @year = @arr[1].split("\"")[3]
+        @year = @arr[1].to_s.split("\"")[3]
         format.csv { send_data @pickups.to_donor_csv(@month, @year, @fileName) }
     end
   end
@@ -23,8 +23,8 @@ class ReportsController < ApplicationController
         @fileName = params[:fileName]
         @pickUpDay = params[:day].to_s.split("\"")[3]
         p @pickUpDay
-        format.csv { render text: @pickups.to_route_csv(@pickupday, @fileName) }
-        format.pdf { send_data @pickups.to_reports_pdf, filename: "pickups.pdf", type: "application/pdf" }
+        format.csv { render text: @pickups.to_route_csv(@pickUpDay, @fileName) }
+        format.pdf { send_data @pickups.to_reports_pdf(@pickUpDay), filename: "pickups.pdf", type: "application/pdf" }
         format.html
     end
   end
--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -3,20 +3,6 @@ class Pickup < ActiveRecord::Base
   validates_presence_of :donor_phone, :donor_name, :donor_address_line1, :donor_zip, :donor_city
   require 'prawn/table'
   
-  #csv for mapquest -- Not sure if RR made this or if someone else needs the
-  #function. RR left in and will evaluate later
-  def self.to_csv(driveDay)
-      attributes = %w(donor_address_line1, donor_city, donor_zip, item_description)
-    CSV.generate(headers: true) do |csv|
-      csv << attributes
-      all.each do |pickup|
-        if pickup.day_id == driveDay
-          csv << attributes.map{ |attr| user.send(attr) }
-        end
-      end
-    end
-  end
-  
 #REPORTS CONTROLS - must be here because it need pickup information  
   #build csv for donor information
   def Pickup.to_donor_csv(month, year, fileName)
@@ -31,11 +17,9 @@ class Pickup < ActiveRecord::Base
         pickMon = pickUpDay[5] + pickUpDay[6]
         if month == pickMon && pickYear == year 
           #... add them to the file - more info on csv stuff at http://www.sitepoint.com/guide-ruby-csv-library-part-2/
-          #we've got some parsing and concatination so let's do it before we add it.
-          lastName = pickup.donor_name.to_s.split(' ')[1]
           fullAddress = "#{pickup.donor_address_line1} #{pickup.donor_address_line2}"
           #spouse is not in the database. Need for sprint 2 or 3
-          csv << [pickup.donor_first_name, "", lastName, fullAddress, pickup.donor_city, "Illinois", pickup.donor_zip, pickup.donor_email, pickup.pickup_time, pickup.item_description]
+          csv << [pickup.donor_first_name, "", pickup.donor_name, fullAddress, pickup.donor_city, "Illinois", pickup.donor_zip, pickup.donor_email, pickup.pickup_time, pickup.item_description]
         end
       end
     end
@@ -46,35 +30,36 @@ class Pickup < ActiveRecord::Base
     #we don't need their unit number to get driving instructions
     CSV.generate(headers: true) do |csv| #generate csv
       #make the headers for the file
-      csv << ['Street', 'City', 'State', 'Zip', 'Notes']
+      csv << ['Street', 'City', 'State', 'Zip']
       all.each do |pickup|
-        p pickup.pickup_time.to_s == pickUpDay
-        #if their pickup day is the same as the one being pulled...
-        #if pickup.pickup_time.to_s == pickUpDay 
-           #... add them to the file - need to add state and country.
-          csv << [pickup.donor_address_line1, pickup.donor_city, "IL", pickup.donor_zip, pickup.item_description]
-        #end
+        #check the pickup day and add if its for the same day
+        if pickup.pickup_time.to_s == pickUpDay.to_s 
+          csv << [pickup.donor_address_line1, pickup.donor_city, "IL", pickup.donor_zip]
+        end
       end
     end
   end
   
   #build the pdf for drivers
-  def Pickup.to_reports_pdf()
+  def Pickup.to_reports_pdf(pickUpDay)
     #set counter variable
     i = 1
     #build pdf
     pdf = Prawn::Document.new
-    #using texts with a table
-    pdf.text "Pickup Schedule for ", :align => :center, :size => 18
+    #header of page
+    pdf.text "Pickups Scheduled for ", :align => :center, :size => 18
     pdf.move_down 8
-    #make the table, grid is really poor syntax and a waste of time. Table is
+    #make the table, grid has really poor syntax and a waste of time. Table is
     #the best, just speficy gem version before publishing
     dataInTable = [["" , "Name/Contact", "Address", "Donor Items/Notes"]]
     all.each do |pickup|
-      dataInTable += [[i, "#{pickup.donor_name}\n#{pickup.donor_phone}", 
-        "#{pickup.donor_address_line1}\n#{pickup.donor_address_line2}\n#{pickup.donor_city}",
-        "#{pickup.item_description}\n#{pickup.other_notes}\n#{pickup.donor_email}"]]
-      i = i+1
+      #check day, then add to pdf
+      if pickup.pickup_time.to_s == pickUpDay.to_s
+        dataInTable += [[i, "#{pickup.donor_first_name} #{pickup.donor_name}\n#{pickup.donor_phone}", 
+          "#{pickup.donor_address_line1}\n#{pickup.donor_address_line2}\n#{pickup.donor_city}",
+          "#{pickup.item_description}\n#{pickup.other_notes}\n#{pickup.donor_email}"]]
+        i = i+1
+      end
     end
     
     pdf.table(dataInTable, :header => true)
